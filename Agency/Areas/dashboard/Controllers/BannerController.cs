de1a1aa3b0406c490582c3d717a5c9c30cf9163b
sananabbasov <sanan_abbasov@list.ru>
2022-06-30 14:32:53 +0400

    06.30.2022

---
+++ Agency/Areas/dashboard/Controllers/BannerController.cs
@@ -0,0 +1,31 @@
+﻿using Agency.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agency.Areas.dashboard.Controllers
+{
+    [Area("dashboard")]
+    public class BannerController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BannerController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var banner = _context.Banners.FirstOrDefault();
+            return View(banner);
+        }
+        public IActionResult Create()
+        {
+            var banner = _context.Banners.ToList();
+            if (banner != null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
